fec7db840c64ad0a72fb803e1973439c1ae1f422
Benaldo Hernandez <Benaldo Hernandez>
2016-03-01 11:37:33 -0500

    add bootstrap and styling to home page

--- ShirtlessAPI/Controllers/HomeController.cs
+++ ShirtlessAPI/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace ShirtlessAPI.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Title = "Home Page";
+            ViewBag.Title = "welcome, dickpigs";
 
             return View();
         }
