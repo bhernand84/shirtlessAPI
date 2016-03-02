dbd3e89577c623a764bcc6f4622ba482dfa47488
Benaldo Hernandez <Benaldo Hernandez>
2016-03-02 05:40:18 -0500

    add spelling suggestions provider

---
+++ ShirtlessAPI/Controllers/AutoCompleteController.cs
@@ -0,0 +1,25 @@
+﻿using ShirtlessAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ShirtlessAPI.Controllers
+{
+    public class SpellingSuggestionsController : ApiController
+    {
+        protected SpellingSuggestionsProvider spellingSuggestionsProvider { get; set; }
+
+        public IEnumerable<string> Get(string query)
+        {
+            return spellingSuggestionsProvider.Get(query);
+        }
+
+        public SpellingSuggestionsController()
+        {
+            spellingSuggestionsProvider = new BingSpellingSuggestionsProvider();
+        }
+    }
+}
---
+++ ShirtlessAPI/Models/BingSpellingSuggestionsProvider.cs
@@ -0,0 +1,37 @@
+﻿using ShirtlessAPI.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+
+namespace ShirtlessAPI.Models
+{
+    public class BingSpellingSuggestionsProvider : SpellingSuggestionsProvider
+    {
+        public IEnumerable<string> Get(string query)
+        {
+            // Create a Bing container.
+
+            string rootUri = "https://api.datamarket.azure.com/Bing/Search";
+
+            var bingContainer = new Bing.BingSearchContainer(new Uri(rootUri));
+
+            // Replace this value with your account key.
+
+            var accountKey = APIKeys.BingKey;
+
+            // Configure bingContainer to use your credentials.
+
+            bingContainer.Credentials = new NetworkCredential(accountKey, accountKey);
+
+            // Build the query.
+
+            var autoCompleteQuery = bingContainer.SpellingSuggestions(query, null, null, null, null, null);
+
+            var completeResults = autoCompleteQuery.Execute();
+
+            return completeResults.Select(m => m.Value);
+        }
+    }
+}
