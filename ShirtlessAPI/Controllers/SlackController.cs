4ce190613d438f092cb868baf1674499e5fed069
Justin Royer <jroyer@tonicdesign.com>
2016-05-09 10:17:17 -0400

    created slack controller and provider

---
+++ ShirtlessAPI/Controllers/SlackController.cs
@@ -0,0 +1,46 @@
+﻿using ShirtlessAPI.Helpers;
+using ShirtlessAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ShirtlessAPI.Controllers
+{
+    public class SlackController : ApiController
+    {
+        protected SearchProvider _searchProvider { get; set; }
+        protected LogProvider _logProvider { get; set; }
+        // GET api/<controller>
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<controller>/5
+        public SearchResult Get(string text)
+        {
+            _logProvider.Log(text);
+            var images = _searchProvider.Get(text);
+
+            return images.FirstOrDefault();
+        }
+
+        [HttpPost]
+        public SearchResult Post(string text)
+        {
+            return _searchProvider.Post(text);
+        }
+
+        #region Constructors
+
+        public SlackController()
+        {
+            _searchProvider = new SlackSearchProvider();
+            _logProvider = new EntityLogProvider();
+        }
+        #endregion
+    }
+}
