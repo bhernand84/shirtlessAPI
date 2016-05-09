e858673ad8dcadc0599e61c01f98bec55b9dc3d1
Justin Royer <jroyer@tonicdesign.com>
2016-05-09 11:02:04 -0400

    created slackSearchResult class

---
+++ ShirtlessAPI/Models/slackSearchResult.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShirtlessAPI.Models
+{
+    public class SlackSearchResult
+    {
+        public string Text { get; set; }
+        public string MediaUrl { get; set; }
+        public string ThumbUrl { get; set; }
+
+        public SlackSearchResult() {}
+
+        public SlackSearchResult(string text, string mediaUrl, string thumbUrl)
+        {
+            Text = text;
+            MediaUrl = mediaUrl;
+            ThumbUrl = thumbUrl;
+        }
+    }
+}
