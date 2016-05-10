6e844953183ed067f07807115effa029e3e71445
Justin Royer <jroyer@tonicdesign.com>
2016-05-10 12:07:07 -0400

    added Payload.cs

---
+++ ShirtlessAPI/Models/Payload.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace ShirtlessAPI.Models
+{
+    public class Payload
+    {
+        [JsonProperty("text")]
+        public string Text { get; set; }
+        [JsonProperty("response_url")]
+        public string ResponseUrl { get; set; }
+
+        public Payload() { }
+
+        public Payload(string text, string responseUrl)
+        {
+            Text = text;
+            ResponseUrl = responseUrl;
+        }
+    }
+}
